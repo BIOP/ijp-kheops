9975fc253baa8ae7f0f179cf95de1ef93e52c8b9
romainGuiet <romain.guiet@epfl.ch>
2019-08-27 14:54:53 +0200

    adds a button to reach documentation on c4science

--- src/main/java/ch/epfl/biop/ij2command/BFConvCommand.java
+++ src/main/java/ch/epfl/biop/ij2command/BFConvCommand.java
@@ -12,9 +12,13 @@ import org.scijava.platform.PlatformService;
 import org.scijava.plugin.Parameter;
 import org.scijava.plugin.Plugin;
 import org.scijava.ui.UIService;
+import org.scijava.widget.Button;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.URL;
+
+import static org.scijava.ItemVisibility.MESSAGE;
 
 /**
  * This example illustrates how to create an ImageJ 2 {@link Command} plugin.
@@ -31,71 +35,81 @@ public class BFConvCommand implements Command {
     UIService uiService;
 
     @Parameter
+    PlatformService ps;
+
+    @Parameter( label = "Read Kheops Documentation", callback = "openDoc", required = false, persist = false )
+    private Button chooseFields;
+
+    @Parameter(label="Select an input file (required)" , required=false)
     File input_path;
 
-    @Parameter(style = "save", required=false, persist=false)
+    @Parameter(label="Specify an output folder (optional)", style = "save", required=false, persist=false)
     File output_dir;
 
-    @Parameter
+    @Parameter(label="Pyramid level(s)")
     int pyramidResolution=2;
 
-    @Parameter
-    int pyramidScale=3;
+    @Parameter(label="Pyramid level downsampling factor")
+    int pyramidScale=4;
 
-    @Parameter
-    int tileXsize=512;
+    @Parameter (label="Individual Tile size (in pixel)")
+    int tileSize=512;
 
-    @Parameter
-    int tileYsize=512;
-
-    @Override
-    public void run() {
-        uiService.show("Hello from the BIOP!");
-        if (!(output_dir == null)){
-            System.out.println( "output set to "+ output_dir.toString() );
-        } else {
-            System.out.println( "output is null " );
-        }
-
-        String fileName = input_path.getName();
-        String fileNameWithOutExt = FilenameUtils.removeExtension( fileName) + ".ome.tiff";
-        File output_path  = new File( "null");
-
-        Boolean isOutputNull = false;
-        if (( output_dir == null) || (output_dir.toString().equals("") )) {
-            isOutputNull = true;
-            File parent_dir =  new File( input_path.getParent() );
-            output_path  = new File(parent_dir, fileNameWithOutExt);
-
-        } else {
-
-            output_dir.mkdirs();
-            output_path  = new File(output_dir, fileNameWithOutExt);
-        }
-
-        String[] params = {  input_path.toString(), output_path.toString() ,
-                            "-pyramid-resolutions",  String.valueOf( pyramidResolution) ,
-                            "-pyramid-scale", String.valueOf(pyramidScale),
-                            "-tilex", String.valueOf(tileXsize),
-                            "-tiley", String.valueOf(tileYsize),
-                            "-noflat"
-                            } ;
-
+    public void openDoc(){
         try {
-            DebugTools.enableLogging("INFO");
-            ImageConverter converter = new ImageConverter();
-            if (!converter.testConvert(new ImageWriter(), params)) System.err.println("Ooups! Something went wrong, contact BIOP team");
-            System.out.println("Jobs Done !");
-        } catch (FormatException e) {
-            e.printStackTrace();
+            // url : go.epfl.ch/ijp-kheops
+            ps.open(new URL("https://c4science.ch/w/bioimaging_and_optics_platform_biop/image-processing/imagej_tools/ijp-kheops/"));
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
 
-        // workaround for batch
-        if (isOutputNull){
-            output_dir = null;
-        }
+
+    @Override
+    public void run() {
+        //uiService.show("Hello from the BIOP!");
+
+            String fileName = input_path.getName();
+            String fileNameWithOutExt = FilenameUtils.removeExtension(fileName) + ".ome.tiff";
+            File output_path = new File("null");
+
+            Boolean isOutputNull = false;
+            if ((output_dir == null) || (output_dir.toString().equals(""))) {
+                isOutputNull = true;
+                File parent_dir = new File(input_path.getParent());
+                output_path = new File(parent_dir, fileNameWithOutExt);
+
+            } else {
+
+                output_dir.mkdirs();
+                output_path = new File(output_dir, fileNameWithOutExt);
+            }
+
+            String[] params = {input_path.toString(), output_path.toString(),
+                    "-pyramid-resolutions", String.valueOf(pyramidResolution),
+                    "-pyramid-scale", String.valueOf(pyramidScale),
+                    "-tilex", String.valueOf(tileSize),
+                    "-tiley", String.valueOf(tileSize),
+                    "-noflat"
+            };
+
+            try {
+                DebugTools.enableLogging("INFO");
+                ImageConverter converter = new ImageConverter();
+                if (!converter.testConvert(new ImageWriter(), params))
+                    System.err.println("Ooups! Something went wrong, contact BIOP team");
+                System.out.println("Jobs Done !");
+            } catch (FormatException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            // workaround for batch
+            if (isOutputNull) {
+                output_dir = null;
+            }
+
     }
 
     /**
