6f07a23e66e69fb24e48228002b5a9118a3c54dd
Nicolas Chiaruttini <nicolas.chiaruttini@epfl.ch>
2021-12-06 15:10:34 +0100

    fix licenses

--- src/main/java/ch/epfl/biop/ij2command/KheopsFromImagePlusCommand.java
+++ src/main/java/ch/epfl/biop/ij2command/KheopsFromImagePlusCommand.java
@@ -1,3 +1,24 @@
+/*-
+ * #%L
+ * IJ2 command make use of bioformats convert to create pyramidal ome.tiff
+ * %%
+ * Copyright (C) 2018 - 2021 ECOLE POLYTECHNIQUE FEDERALE DE LAUSANNE, Switzerland, BioImaging And Optics Platform (BIOP)
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 2 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/gpl-2.0.html>.
+ * #L%
+ */
 package ch.epfl.biop.ij2command;
 
 import ch.epfl.biop.ImagePlusToOMETiff;
