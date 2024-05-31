4f2229ace2f280ccccf4eaa090c4df90595bec0f
Nicolas Chiaruttini <nicolas.chiaruttini@epfl.ch>
2024-05-31 10:53:17 +0200

    fix licenses

--- src/main/java/ch/epfl/biop/kheops/SourceAndConverterFromSpimDataCreator.java
+++ src/main/java/ch/epfl/biop/kheops/SourceAndConverterFromSpimDataCreator.java
@@ -3,7 +3,7 @@ package ch.epfl.biop.kheops;
  * #%L
  * IJ2 commands that use bio-formats to create pyramidal ome.tiff
  * %%
- * Copyright (C) 2018 - 2023 ECOLE POLYTECHNIQUE FEDERALE DE LAUSANNE, Switzerland, BioImaging And Optics Platform (BIOP)
+ * Copyright (C) 2018 - 2024 ECOLE POLYTECHNIQUE FEDERALE DE LAUSANNE, Switzerland, BioImaging And Optics Platform (BIOP)
  * %%
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as
